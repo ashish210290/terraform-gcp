c31aa175daa8045c8104560615ee2d6290d67edc
ashish <ashish@lyryx.com>
2023-11-08 15:19:16 -0700

    added terraform apply trigger

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,3 +21,31 @@ resource "google_cloudbuild_trigger" "test" {
     }
     included_files = ["terraform/**"]
 }
+
+
+# Create Trigger for terraform apply ##
+
+
+resource "google_cloudbuild_trigger" "trigger-apply" {
+     provider = google-beta
+     project = "divine-energy-253221"
+     name = "Terraform-apply-${var.env}"
+     description = "A trigger to apply terraform on git push to main"
+
+    github {
+      name = "terraform-gcp"
+      owner = "ashish210290"
+      push {
+        branch = "^${var.data_platform_ops_br}$"
+      }
+    }
+    filename = "cloud-build/tf-apply-project.yaml"
+    substitutions = {
+      _BACKEND_CONFIG_PREFIX: "terraform/${var.env}"
+      _TF_COMMAND = "apply"
+      _TF_OPTION = "-auto-approve"
+      _VAR_FILES = "../tfvars/pr.tfvars"
+      _TF_EXTRA_OPTION = "-lock=false"
+    }
+    included_files = ["terraform/**"]
+}
