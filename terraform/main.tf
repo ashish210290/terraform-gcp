b9e64594c69ebf3eebcf68063ad12326305500e1
ashish <ashish@lyryx.com>
2023-11-07 19:37:15 -0700

    resructure vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,9 @@
 resource "google_cloudbuild_trigger" "test" {
      provider = google-beta
      project = "divine-energy-253221"
-     name = "Terraform-Trigger"
+     name = "Terraform-plan-${var.env}"
      description = "A trigger to push to any branch"
-    # trigger_template {
-    #   branch_name = "np"
-    #   repo_name = "ashish210290/terraform-gcp"
-    # }
+
     github {
       name = "terraform-gcp"
       owner = "ashish210290"
@@ -14,11 +11,11 @@ resource "google_cloudbuild_trigger" "test" {
         branch = "^main$"
       }
     }
-    filename = "cloud-build/tf-apply-project.yaml"
+    filename = "cloud-build/tf-plan-project.yaml"
     substitutions = {
       _TF_COMMAND = "plan"
       _TF_OPTION = "-auto-approve"
-      _VAR_FILES = "pr.tfvars"
+      _VAR_FILES = "tfvars/pr.tfvars"
       _TF_EXTRA_OPTION = "-lock=false"
     }
     included_files = ["terraform/**"]
---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "env" {}
+variable "project_id" {}
+variable "project_name" {}
+variable "project_number" {}
+variable "region" {}
+variable "data_platform_ops_br" {}
