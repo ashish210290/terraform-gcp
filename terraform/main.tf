84c795e6770c09e50558286b4821d5c0c9e7a018
ashish <ashish@lyryx.com>
2024-06-14 09:49:46 -0600

    knkl

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,150 +1,150 @@
-# Create Trigger for terraform plan ##
+# # Create Trigger for terraform plan ##
 
 
-variable "enable_nifi_alert" {
-    description = "Enable nifi metric alerts"
-    type = string
-    default = "true"
+# variable "enable_nifi_alert" {
+#     description = "Enable nifi metric alerts"
+#     type = string
+#     default = "true"
   
-}
-provider "google-beta" {
-  project = var.project_id
-  region = "northamerica-northeast1"
-}
-resource "google_cloudbuild_trigger" "test" {
-     provider = google-beta
-     project = "divine-energy-253221"
-     name = "Terraform-plan-${var.env}"
-     description = "A trigger to push to any branch"
+# }
+# provider "google-beta" {
+#   project = var.project_id
+#   region = "northamerica-northeast1"
+# }
+# resource "google_cloudbuild_trigger" "test" {
+#      provider = google-beta
+#      project = "divine-energy-253221"
+#      name = "Terraform-plan-${var.env}"
+#      description = "A trigger to push to any branch"
 
-    github {
-      name = "terraform-gcp"
-      owner = "ashish210290"
-      push {
-        branch = "^main$"
-      }
-    }
-    filename = "cloud-build/tf-plan-project.yaml"
-    substitutions = {
-      _BACKEND_CONFIG_PREFIX: "terraform/${var.env}"
-      _TF_COMMAND = "plan"
-      _TF_OPTION = "-auto-approve"
-      _VAR_FILES = "../tfvars/pr.tfvars"
-      _TF_EXTRA_OPTION = "-lock=false"
-    }
-    approval_config {
-      approval_required = false
-    }
-    included_files = ["terraform/**"]
-}
-
-
-# Create Trigger for terraform apply, approval required ##
+#     github {
+#       name = "terraform-gcp"
+#       owner = "ashish210290"
+#       push {
+#         branch = "^main$"
+#       }
+#     }
+#     filename = "cloud-build/tf-plan-project.yaml"
+#     substitutions = {
+#       _BACKEND_CONFIG_PREFIX: "terraform/${var.env}"
+#       _TF_COMMAND = "plan"
+#       _TF_OPTION = "-auto-approve"
+#       _VAR_FILES = "../tfvars/pr.tfvars"
+#       _TF_EXTRA_OPTION = "-lock=false"
+#     }
+#     approval_config {
+#       approval_required = false
+#     }
+#     included_files = ["terraform/**"]
+# }
 
 
-resource "google_cloudbuild_trigger" "trigger-apply" {
-     provider = google-beta
-     project = "divine-energy-253221"
-     name = "Terraform-apply-${var.env}"
-     description = "A trigger to apply terraform on git push to main"
+# # Create Trigger for terraform apply, approval required ##
 
-    github {
-      name = "terraform-gcp"
-      owner = "ashish210290"
-      push {
-        branch = "^${var.data_platform_ops_br}$"
-      }
-    }
-    filename = "cloud-build/tf-apply-project.yaml"
-    substitutions = {
-      _BACKEND_CONFIG_PREFIX: "terraform/${var.env}"
-      _TF_COMMAND = "apply"
-      _TF_OPTION = "-auto-approve"
-      _VAR_FILES = "../tfvars/pr.tfvars"
-      _TF_EXTRA_OPTION = "-lock=false"
-    }
-    approval_config {
-      approval_required = true
-    }
-    included_files = ["terraform/**"]
-}
 
-resource "google_monitoring_alert_policy" "test_alert_policy_name" {
+# resource "google_cloudbuild_trigger" "trigger-apply" {
+#      provider = google-beta
+#      project = "divine-energy-253221"
+#      name = "Terraform-apply-${var.env}"
+#      description = "A trigger to apply terraform on git push to main"
+
+#     github {
+#       name = "terraform-gcp"
+#       owner = "ashish210290"
+#       push {
+#         branch = "^${var.data_platform_ops_br}$"
+#       }
+#     }
+#     filename = "cloud-build/tf-apply-project.yaml"
+#     substitutions = {
+#       _BACKEND_CONFIG_PREFIX: "terraform/${var.env}"
+#       _TF_COMMAND = "apply"
+#       _TF_OPTION = "-auto-approve"
+#       _VAR_FILES = "../tfvars/pr.tfvars"
+#       _TF_EXTRA_OPTION = "-lock=false"
+#     }
+#     approval_config {
+#       approval_required = true
+#     }
+#     included_files = ["terraform/**"]
+# }
+
+# resource "google_monitoring_alert_policy" "test_alert_policy_name" {
   
-  project = "divine-energy-253221"
-  display_name = "My Alert Policy"
-  combiner     = "OR"
+#   project = "divine-energy-253221"
+#   display_name = "My Alert Policy"
+#   combiner     = "OR"
 
-  conditions {
-    display_name = "test condition - metrics missing"
-    condition_absent {
-      filter = "resource.type = \"gce_instance\" AND metric.type = \"compute.googleapis.com/instance/cpu/utilization\""
-      duration = "300s"
+#   conditions {
+#     display_name = "test condition - metrics missing"
+#     condition_absent {
+#       filter = "resource.type = \"gce_instance\" AND metric.type = \"compute.googleapis.com/instance/cpu/utilization\""
+#       duration = "300s"
 
-      trigger {
-        count = 1
-      }
+#       trigger {
+#         count = 1
+#       }
       
-      aggregations {
-        alignment_period = "300s"
-        per_series_aligner = "ALIGN_MEAN"
-      }
-    }
-  }
-  enabled = true
-  notification_channels = ["projects/divine-energy-253221/notificationChannels/16399540197443471345"]
-}
+#       aggregations {
+#         alignment_period = "300s"
+#         per_series_aligner = "ALIGN_MEAN"
+#       }
+#     }
+#   }
+#   enabled = true
+#   notification_channels = ["projects/divine-energy-253221/notificationChannels/16399540197443471345"]
+# }
 
-resource "google_monitoring_alert_policy" "Alert-Policy-1" {
-    project = "divine-energy-253221"
-    display_name = "My Alert POlicy 1"
-    combiner = "OR"
-    conditions {
-      display_name = "Test Conditions 1"
-      condition_threshold {
-      filter = "resource.type = \"gce_instance\" AND metric.type = \"compute.googleapis.com/instance/cpu/utilization\""
-      duration = "60s"
-      comparison = "COMPARISON_GT"
-      aggregations {
-        alignment_period = "60s"
-        per_series_aligner = "ALIGN_NONE"
-      }
-      evaluation_missing_data = "EVALUATION_MISSING_DATA_INACTIVE"
-      }     
-    }
-    user_labels = {
-      foo = "bar"
-    }
-}
+# resource "google_monitoring_alert_policy" "Alert-Policy-1" {
+#     project = "divine-energy-253221"
+#     display_name = "My Alert POlicy 1"
+#     combiner = "OR"
+#     conditions {
+#       display_name = "Test Conditions 1"
+#       condition_threshold {
+#       filter = "resource.type = \"gce_instance\" AND metric.type = \"compute.googleapis.com/instance/cpu/utilization\""
+#       duration = "60s"
+#       comparison = "COMPARISON_GT"
+#       aggregations {
+#         alignment_period = "60s"
+#         per_series_aligner = "ALIGN_NONE"
+#       }
+#       evaluation_missing_data = "EVALUATION_MISSING_DATA_INACTIVE"
+#       }     
+#     }
+#     user_labels = {
+#       foo = "bar"
+#     }
+# }
 
- resource "google_monitoring_alert_policy" "nifi_jvm_metrics_status" {
-  project               = "divine-energy-253221"
-  display_name          = "[${var.env}] NiFi JVM is down for 5 mins"
-  documentation {
-    content = "Either Prod NiFi instance is down or its bindplane agent is not running"
-    mime_type = "text/markdown"
-  }
-  severity = "CRITICAL"
-  notification_channels = ["projects/divine-energy-253221/notificationChannels/16399540197443471345"]
-  combiner              = "OR"
-  enabled               = var.enable_nifi_alert
+#  resource "google_monitoring_alert_policy" "nifi_jvm_metrics_status" {
+#   project               = "divine-energy-253221"
+#   display_name          = "[${var.env}] NiFi JVM is down for 5 mins"
+#   documentation {
+#     content = "Either Prod NiFi instance is down or its bindplane agent is not running"
+#     mime_type = "text/markdown"
+#   }
+#   severity = "CRITICAL"
+#   notification_channels = ["projects/divine-energy-253221/notificationChannels/16399540197443471345"]
+#   combiner              = "OR"
+#   enabled               = var.enable_nifi_alert
 
-  conditions {
-    display_name = "NiFi is down - prometheus/nifi_jvm_uptime/gauge is missing"
-    condition_absent {
-      filter   = "resource.type = \"gce_instance\" AND metric.type = \"compute.googleapis.com/instance/cpu/utilization\""
-      duration = "300s"
-      trigger {
-        count = 1
-      }
-      aggregations {
-        alignment_period     = "300s"
-        per_series_aligner   = "ALIGN_MEAN"
-        cross_series_reducer = "REDUCE_NONE"
-      }
-    }
-  }
-}
+#   conditions {
+#     display_name = "NiFi is down - prometheus/nifi_jvm_uptime/gauge is missing"
+#     condition_absent {
+#       filter   = "resource.type = \"gce_instance\" AND metric.type = \"compute.googleapis.com/instance/cpu/utilization\""
+#       duration = "300s"
+#       trigger {
+#         count = 1
+#       }
+#       aggregations {
+#         alignment_period     = "300s"
+#         per_series_aligner   = "ALIGN_MEAN"
+#         cross_series_reducer = "REDUCE_NONE"
+#       }
+#     }
+#   }
+# }
 
 # Create three Regional Disks 
 
@@ -238,32 +238,27 @@ resource "null_resource" "wait_for_shutdown" {
 }
 
 
-# deattach regional disks from temporary disk-formatter instances
+# # deattach regional disks from temporary disk-formatter instances
 
-resource "google_compute_instance" "disk-formatter-deattach" {
-  count = 3
-  name    = google_compute_instance.disk-formatter[count.index].name
-  machine_type = google_compute_instance.disk-formatter[count.index].machine_type
-  zone = google_compute_instance.disk-formatter[count.index].zone
-  scheduling {
-    preemptible       = true
-    automatic_restart = false
-    on_host_maintenance = "TERMINATE"
-  }
+# resource "google_compute_instance" "disk-formatter-deattach" {
+#   count = 3
+#   name    = google_compute_instance.disk-formatter[count.index].name
+#   machine_type = google_compute_instance.disk-formatter[count.index].machine_type
+#   zone = google_compute_instance.disk-formatter[count.index].zone
 
-  boot_disk {
-    source = google_compute_instance.disk-formatter[count.index].boot_disk[0].source
-  }
+#   boot_disk {
+#     source = google_compute_instance.disk-formatter[count.index].boot_disk[0].source
+#   }
 
-  network_interface {
-   network = "default"
-  }
+#   network_interface {
+#    network = "default"
+#   }
 
-  lifecycle {
-    prevent_destroy = false
-  }
-  depends_on = [ null_resource.wait_for_shutdown ]
-}
+#   lifecycle {
+#     prevent_destroy = false
+#   }
+#   depends_on = [ null_resource.wait_for_shutdown ]
+# }
 
 #  ##Create an instance template 0
 
