2615a9b404fe7b309e28048ac37c4092bd86a8c7
ashish <ashish@lyryx.com>
2024-06-18 12:44:14 -0600

    target=1

--- terraform/main.tf
+++ terraform/main.tf
@@ -393,7 +393,7 @@ resource "google_compute_instance_group_manager" "instance-group-manager" {
   name = "sftp-instance-group-manager-${count.index}"
   base_instance_name = "sftp-instance"
   zone = "northamerica-northeast1-a"
-  target_size = 3
+  target_size = 1
 
   version {
     instance_template = google_compute_instance_template.instance_template_1[count.index].id
