b2c582132bc190b758dc2c997387970c2c663561
ashish <ashish@lyryx.com>
2024-06-25 01:51:04 -0600

    1 container

--- terraform/main.tf
+++ terraform/main.tf
@@ -306,19 +306,11 @@ resource "google_compute_instance_template" "instance_template_0" {
               mountPath: /mnt/sftpgo  
               mountPropagation: "Bidirectional"
 
-          - name: sftpgo
-            image: drakkan/sftpgo
-            volumeMounts:
-              - mountPath: /var/lib/sftpgo
-                name: sftpgo-vol
         volumes:
           - name: dev-fuse
             mountPath: /dev/fuse
           - name: sftpgo-gcsfuse-share
             mountPath: /mnt/disks/sftpgo
-          - name: sftpgo-vol
-            hostPath:
-              path: /mnt/disks/sftpgo
     EOF
     user-data = <<-EOF
       #cloud-config
@@ -402,6 +394,89 @@ resource "google_compute_health_check" "default" {
 
 
 
+# # Create an instance template
+# resource "google_compute_instance_template" "instance_template_0" {
+#   #count = 3
+#   name_prefix           = "sftpgo-instance-template-"
+#   machine_type   = "e2-micro"
+
+#   scheduling {
+#     automatic_restart   = true
+#     on_host_maintenance = "MIGRATE"
+#   }
+    
+#   disk {
+#     auto_delete  = true
+#     boot         = true
+#     source_image = "projects/cos-cloud/global/images/family/cos-stable"  # Container-Optimized OS
+#     disk_type = "pd-standard"
+#     disk_size_gb = 20
+#   }
+
+#   network_interface {
+#     network = "default"
+
+#     access_config {
+      
+#     }
+#   }
+
+#   metadata = {
+#     gce-container-declaration = <<-EOF
+#       spec:
+#         containers:
+#           - name: gcs-fuse
+#             image:  northamerica-northeast1-docker.pkg.dev/divine-energy-253221/gcp-repo/gcs-bucket-mount
+#             securityContext:
+#               privileged: true
+#             volumeMounts:
+#             - name: dev-fuse
+#               mountPath: /dev/fuse
+#             - name: sftpgo-gcsfuse-share
+#               mountPath: /mnt/sftpgo  
+#               mountPropagation: "Bidirectional"
+
+#           - name: sftpgo
+#             image: drakkan/sftpgo
+#             volumeMounts:
+#               - mountPath: /var/lib/sftpgo
+#                 name: sftpgo-vol
+#         volumes:
+#           - name: dev-fuse
+#             mountPath: /dev/fuse
+#           - name: sftpgo-gcsfuse-share
+#             mountPath: /mnt/disks/sftpgo
+#           - name: sftpgo-vol
+#             hostPath:
+#               path: /mnt/disks/sftpgo
+#     EOF
+#     user-data = <<-EOF
+#       #cloud-config
+#       runcmd:
+#       - |
+#         #!/bin/bash
+          
+#         mkdir -p "/mnt/disks/sftpgo"
+        
+#         chmod 777 /mnt/disks/sftpgo
+
+#       bootcmd:
+#       - mkdir -p "/mnt/disks/sftpgo"
+#       - chmod 777 /mnt/disks/sftpgo
+#     EOF 
+#   }
+
+#   service_account {
+#     email  = "default"
+#     scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+#   }
+
+#   tags = ["http-server"]
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
 
 
