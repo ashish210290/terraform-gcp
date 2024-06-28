77220298cc16a637cbbc6b31fda36a4d83c8dd43
ashish <ashish@lyryx.com>
2024-06-28 02:24:26 -0600

    Added max_connections_per_instance for balancing_mode

--- terraform/main.tf
+++ terraform/main.tf
@@ -440,6 +440,7 @@ resource "google_compute_backend_service" "nlb-backend-service-0" {
   backend {
     group = google_compute_instance_group_manager.instance-group-manager-0.instance_group
     balancing_mode = "CONNECTION"
+    max_connections_per_instance = 100
   }
   log_config {
     enable = true
