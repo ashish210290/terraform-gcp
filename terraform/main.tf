648b1533e122dbc2781ebe52ab3602a8b974c3d7
ashish <ashish@lyryx.com>
2024-07-15 12:14:18 -0600

    Added scope https://www.googleapis.com/auth/cloud-platform

--- terraform/main.tf
+++ terraform/main.tf
@@ -341,7 +341,8 @@ resource "google_compute_instance_template" "instance_template_0" {
 
   service_account {
     email  = "sftpgo-sa@divine-energy-253221.iam.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/servicecontrol",
+        scopes = ["https://www.googleapis.com/auth/cloud-platform",
+              "https://www.googleapis.com/auth/servicecontrol",
               "https://www.googleapis.com/auth/service.management.readonly",
               "https://www.googleapis.com/auth/logging.write",
               "https://www.googleapis.com/auth/monitoring.write",
