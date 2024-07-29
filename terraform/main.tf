8cc72a11f963d3e28eb6f6bf7a6dbc0f7c0bf7bd
ashish <ashish@lyryx.com>
2024-07-29 13:54:02 -0600

    wd

--- terraform/main.tf
+++ terraform/main.tf
@@ -926,6 +926,7 @@ resource "google_compute_region_health_check" "sftpgo-health-http-check" {
   #----------------------------------------------#
 
 resource "google_compute_region_backend_service" "nlb-backend-service-0" {
+  count = 1
   name = "nlb-backend-service-0"
   region = "northamerica-northeast1"
   health_checks = [google_compute_region_health_check.sftpgo-health-http-check.id]
@@ -936,7 +937,7 @@ resource "google_compute_region_backend_service" "nlb-backend-service-0" {
   #locality_lb_policy = "MAGLEV"
   session_affinity = "CLIENT_IP_PROTO"
   backend {
-    group = google_compute_instance_group_manager.instance-group-manager-0[0].id
+    group = google_compute_instance_group_manager.instance-group-manager-0[count.index].id
     balancing_mode = "CONNECTION"
     #max_connections_per_instance = 100
     
