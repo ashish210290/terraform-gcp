2558ea9a05de903aa4467722671fa708eb02af68
ashish <ashish@lyryx.com>
2024-07-31 14:10:20 -0600

    gfj

--- terraform/main.tf
+++ terraform/main.tf
@@ -931,8 +931,8 @@ resource "google_compute_region_backend_service" "nlb-backend-service-0" {
   backend {
     group = google_compute_instance_group_manager.instance-group-manager.instance_group
     balancing_mode = "CONNECTION"
-    #max_connections = 10
-    max_connections_per_instance = 10
+    max_connections = 100
+    #max_connections_per_instance = 10
     
   }
   log_config {
