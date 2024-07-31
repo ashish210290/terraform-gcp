223358a41b8e12622cdbbff57395dbd4daffacf4
ashish <ashish@lyryx.com>
2024-07-31 16:35:31 -0600

    oihil

--- terraform/main.tf
+++ terraform/main.tf
@@ -948,11 +948,11 @@ resource "google_compute_region_backend_service" "nlb-backend-service-0" {
 #------------------------------------------
 
 
-resource "google_compute_target_tcp_proxy" "tcp_proxy" {
-  name = "sftpgo-nlb-target-proxy"
-  proxy_header = "NONE"
-  backend_service = google_compute_region_backend_service.nlb-backend-service-0.self_link
-}
+# resource "google_compute_target_tcp_proxy" "tcp_proxy" {
+#   name = "sftpgo-nlb-target-proxy"
+#   proxy_header = "NONE"
+#   backend_service = google_compute_region_backend_service.nlb-backend-service-0.self_link
+# }
   #------------------------------------------------------------#
   # iv. Create Forwarding rules for SftpGo ports 22 and 8080 |
   #------------------------------------------------------------#
@@ -963,7 +963,7 @@ resource "google_compute_forwarding_rule" "tcp8080-22-forwarding-rule" {
   backend_service = google_compute_region_backend_service.nlb-backend-service-0.id
   ip_address = "10.162.0.10"
   ports = [ "22", "8080" ]
-  target = google_compute_target_tcp_proxy.tcp_proxy.id
+  #target = google_compute_target_tcp_proxy.tcp_proxy.id
   ip_protocol = "TCP"
   ip_version = "IPV4"
   load_balancing_scheme = "INTERNAL_MANAGED"
